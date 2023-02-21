5b84ac01fd0ddafc296453298c7a150a58a110ed
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-02-21 16:13:17 -0600

    Add\ Footer

--- resources/views/front/_partials/footer.blade.php
+++ resources/views/front/_partials/footer.blade.php
@@ -3,14 +3,14 @@
     <div class="bg-main relative py-20 px-4 flex md:flex-row md:px-6 sm:justify-center flex-col items-center gap-24">
 
         <div>
-            <img src="{{ asset('assets/log-alt.png') }}" alt="Logo Country Hotel">
+            <img src="{{ asset('assets/logo-alt.png') }}" alt="Logo Country Hotel">
         </div>
 
         <div class="sm:order-3">
             <ul class="grid grid-cols-2 gap-7">
-                <li><a target="_blank" href="tel:3338230492"><img src="{{ asset('assets/icons/socials/WhatsApp Inc.svg') }}" alt="WhatsApp"></a></li>
-                <li><a target="_blank" href="https://www.facebook.com/CountryHotelSuites/"><img src="{{ asset('assets/icons/socials/Facebook.svg') }}" alt="Facebook"></a></li>
-                <li class="col-span-2 mx-auto"><a target="_blank" href="https://www.instagram.com/countryhotelsuites/"><img src="{{ asset('assets/icons/socials/Instagram.svg') }}" alt="Instagram"></a></li>
+                <li><a target="_blank" href="https://api.whatsapp.com/send?phone=523322415915&text=Hola,%20necesito%20ayuda"><img src="{{ asset('assets/icons/socials/WhatsApp Inc.svg') }}" alt="WhatsApp"></a></li>
+                <li><a target="_blank" href="https://www.facebook.com/HotelIsabelMx"><img src="{{ asset('assets/icons/socials/Facebook.svg') }}" alt="Facebook"></a></li>
+                <li class="col-span-2 mx-auto"><a target="_blank" href="https://www.instagram.com/hotelisabelgdl/"><img src="{{ asset('assets/icons/socials/Instagram.svg') }}" alt="Instagram"></a></li>
             </ul>
         </div>
 
@@ -22,13 +22,17 @@
             <div class="flex flex-col gap-16 text-left font-bold">
                 <a class="flex gap-2" href="tel:+523338230463">
                     <img src="{{ asset('assets/icons/call-outline.svg') }}" alt="Location Icon">
-                    <span>+52 33 3823 0463</span>
+                    <span>+52 33 3826 2630</span>
+                </a>
+                <a class="flex gap-2" href="mailto:523338230463">
+                    <img src="{{ asset('assets/icons/services/mail-outline.svg') }}" alt="Location Icon">
+                    <span>reservaciones@hotelisabel.com</span>
                 </a>
                 <a class="flex gap-2" href="#">
                     <img src="{{ asset('assets/icons/location-outline.svg') }}" alt="Location Icon">
                     <span>
-                        Av. Circ. Jorge Álvarez del Castillo No 1482
-                        Col. Chapultepec Country C.P. 44620
+                        José Gpe. Montenegro #1572
+                        44170 Guadalajara, Jalisco, México
                     </span>
                 </a>
             </div>
@@ -39,11 +43,11 @@
     <div class="bg-main-dark p-4">
 
         <ul class="text-gray font-bold flex flex-wrap gap-2 justify-center">
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Instalaciones</a></li>
-            <li><a href="#">Habitaciones</a></li>
-            <li><a href="#">Servicios</a></li>
-            <li><a href="#">Contacto</a></li>
+            <li><a href="{{route('home')}}">Inicio</a></li>
+            <li><a href="{{route('installations')}}">Instalaciones</a></li>
+            <li><a href="{{route('rooms')}}">Habitaciones</a></li>
+            <li><a href="{{route('services')}}">Servicios</a></li>
+            <li><a href="{{route('contact')}}">Contacto</a></li>
         </ul>
 
     </div>
@@ -53,7 +57,7 @@
             &copy; 
             <script>document.write(new Date().getFullYear())</script>
             <noscript>2023</noscript>
-            Country Hotel & Suites. Todos los derechos reservados.
+            Hotel Isabel. Todos los derechos reservados.
             Powered by <a href="https://pcbtroniks.com" target="_blank" class="font-bold">Pcbtroniks</a>.
         </p>
     </div>
