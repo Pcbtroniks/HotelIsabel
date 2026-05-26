6012c584b7c2b007a3d1a30f55da803c1bf6b728
JeanLanma <jeanmacario048@gmail.com>
2026-05-26 16:07:27 -0600

    ADD\ Reglamentos y menu

---
+++ resources/views/front/menus/sections/links.blade.php
@@ -0,0 +1,65 @@
+<section>
+
+    <div class="px-4 flex flex-col items-center pb-16">
+
+        <a target="_blank" href="{{ route('menus.habitaciones.file') }}" class="bg-white flex items-center my-5 py-4 px-2 sm:px-4 rounded-full shadow-card-hard sm:w-[29.25rem] w-[20.5rem]">
+            <div class="w-[4.5rem]">
+                <img class="w-full" src="{{ asset('assets/icons/socials/logo-restaurante-isabel.png') }}" alt="Logo restaurant">
+            </div>
+            <div class="ml-2 sm:ml-6">
+                <h2 class="sm:text-sm uppercase font-medium">Menu Habitaciones</h2>
+                <p class="text-xs">Descubre nuestros menús de habitaciones.</p>
+            </div>
+        </a>
+
+        <a target="_blank" href="{{ route('menus.canales.file') }}" class="bg-white flex items-center my-5 py-4 px-2 sm:px-4 rounded-full shadow-card-hard sm:w-[29.25rem] w-[20.5rem]">
+            <div class="w-[4.5rem]">
+                <img class="w-full rounded-full" src="{{ asset('assets/icons/socials/mosaico-1.png') }}" alt="Logo restaurant">
+            </div>
+            <div class="ml-2 sm:ml-6">
+                <h2 class="sm:text-sm uppercase font-medium">Nuestros canales de televisión</h2>
+                <p class="text-xs">Descubre nuestra carta de canales.</p>
+            </div>
+        </a>
+
+        <a target="_blank" href="{{ route('menus.reglamento.interno.file') }}" class="bg-white flex items-center my-5 py-4 px-2 sm:px-4 rounded-full shadow-card-hard sm:w-[29.25rem] w-[20.5rem]">
+            <div class="w-[4.5rem]">
+                <img class="w-full rounded-full" src="{{ asset('assets/icons/socials/mosaico-2.png') }}" alt="Logo Hotel Isabel Mosaico">
+            </div>
+            <div class="ml-2 sm:ml-6">
+                <h2 class="sm:text-sm uppercase font-medium">Reglamento interno</h2>
+            </div>
+        </a>
+
+        <a target="_blank" href="{{ route('menus.reglamento.mascotas.file') }}" class="bg-white flex items-center my-5 py-4 px-2 sm:px-4 rounded-full shadow-card-hard sm:w-[29.25rem] w-[20.5rem]">
+            <div class="w-[4.5rem]">
+                <img class="w-full rounded-full" src="{{ asset('assets/icons/socials/mosaico-1.png') }}" alt="Logo Hotel Isabel Mosaico">
+            </div>
+            <div class="ml-2 sm:ml-6">
+                <h2 class="sm:text-sm uppercase font-medium">Reglamento mascotas</h2>
+            </div>
+        </a>
+
+        <a target="_blank" href="https://www.facebook.com/hotelisabelgdl/" class="bg-white flex items-center my-5 py-4 px-2 sm:px-4 rounded-full shadow-card-hard sm:w-[29.25rem] w-[20.5rem]">
+            <div class="w-[4.5rem]">
+                <img class="w-full" src="{{ asset('assets/icons/socials/facebook-color.svg') }}" alt="Logo restaurant">
+            </div>
+            <div class="ml-2 sm:ml-6">
+                <h2 class="sm:text-sm uppercase font-medium">¡Siguenos!</h2>
+                <p class="text-xs">Siguenos en redes sociales, y descubre todos los servicios que tenemos para ti.</p>
+            </div>
+        </a>
+
+        <a target="_blank" href="https://www.instagram.com/hotelisabelgdl/" class="bg-white flex items-center my-5 py-4 px-2 sm:px-4 rounded-full shadow-card-hard sm:w-[29.25rem] w-[20.5rem]">
+            <div class="w-[4.5rem]">
+                <img class="w-full" src="{{ asset('assets/icons/socials/instagram-color.svg') }}" alt="Logo restaurant">
+            </div>
+            <div class="ml-2 sm:ml-6">
+                <h2 class="sm:text-sm uppercase font-medium">¡Comparte Grandes Momentos!</h2>
+                <p class="text-xs">No olvides compartir tus historias con nosotros. #HotelIsabel</p>
+            </div>
+        </a>
+    
+    </div>
+
+</section>
